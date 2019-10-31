b76b1bc09afcf8aa454b71bdd5e5084407e42ab7
Sachin Shinde <sachinshinde7676@gmail.com>
2019-10-31 23:54:33 +0530

    [Logging enhancements][added methods to set logging contexts and logging levels]

--- lib/socketclusterclient/log.rb
+++ lib/socketclusterclient/log.rb
@@ -44,4 +44,27 @@ module Log
   def enable_logging
     initialize_logger
   end
+
+
+  #
+  # Method to set logging level
+  #
+  #
+  #
+  #
+  def set_logging_level(level)
+    # need to set the logging level, create warning if logging level is invalid
+  end
+
+
+  #
+  # Method Create logging context
+  #
+  #
+  #
+  #
+  def set_logging_context
+    # create logging context, which can carry logging information throughout the thread...
+  end
+
 end
